c828bb79781bcd33403af961e7b55b040be06aa6
Tasavvuf Gori <tasavvufg@gmail.com>
2026-04-18 11:29:58 +0530

    feat: replace counter app with search layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import React, { useState } from 'react'
-import { decrement, inceaseBynum, increment } from './redux/features/counter'
-import { useDispatch, useSelector } from 'react-redux'
+import ResultGrid from "./components/ResultGrid"
+import Searchbar from "./components/Searchbar"
+import Tabs from "./components/Tabs"
 
 function App() {
-  const dispach = useDispatch()
-    const count = useSelector((state) => state.counter.value)
-  const [num, setnum] = useState(3)
+
   return (
-    <div className='bg-black h-dvh flex flex-col  justify-center items-center gap-5 text-white '>
-      <h1 className='text-xl' >{count}</h1>
-      <button className='bg-gray-400 p-3 rounded'  onClick={()=>dispach(increment())}>i</button>
-      <button  className='bg-gray-400 p-3 rounded' onClick={()=>dispach(decrement())} >d</button>
-      <input type="text" value={num} onChange={(e)=>{
-        setnum(e.target.value)
-      }}/>
-      <button className='bg-gray-400 p-3 rounded' onClick={()=>{
-        dispach(inceaseBynum(Number(num)))
-      }}>increase by input</button>
+    <div className=' px-6 min-h-screen w-full text-white bg-black ' >
+      <Searchbar/>
+      <Tabs />
+      <ResultGrid/>
     </div>
   )
 }
